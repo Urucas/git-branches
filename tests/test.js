3242dde15d8e49cb7b2285fe03ad526c039299bc
vrunoa <vruno.alassia@gmail.com>
2016-01-29 11:33:04 -0800

    update lib, set git root to process.cwd() if null is passed

--- tests/test.js
+++ tests/test.js
@@ -4,9 +4,18 @@ import chai from 'chai'
 let should = chai.should()
 
 describe("Test brances.js", () => {
-  
+ 
   it("Test branches result", (done) => {
     gitBranches(process.cwd()).then( (branches) => {
+      branches.should.be.a("array")
+      branches.indexOf("master").should.not.equal(-1)
+      done()
+    })
+  })
+
+
+  it("Test branches root is set to cwd if null", (done) => {
+    gitBranches().then( (branches) => {
       branches.should.be.a("array")
       branches.indexOf("master").should.not.equal(-1)
       done()
